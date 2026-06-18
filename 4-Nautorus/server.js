0518cf4f4876668539866668b3bbd27bf914073f
Gante1802 <diegosolergomez95@gmail.com>
2026-06-18 13:09:48 -0400

    .

--- 4-Nautorus/server.js
+++ 4-Nautorus/server.js
@@ -1,8 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './confing.env' });
-
 const app = require('./app');
+
+dotenv.config({ path: './confing.env' });
 console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '#Diego1802#',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('Database connection successful');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const TOUR = mongoose.model('Tour', tourSchema);
+const testTour = new TOUR({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console('Error');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
